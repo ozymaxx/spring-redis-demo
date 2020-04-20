068c3e7286bb16c27bb9dd7ba69f92c29a0228c0
Ozan Can Altıok <ozymaxx@gmail.com>
2020-04-20 21:53:57 +0200

    initial commit

---
+++ src/test/java/com/example/ozymaxx/redisdemo/RedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ozymaxx.redisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
