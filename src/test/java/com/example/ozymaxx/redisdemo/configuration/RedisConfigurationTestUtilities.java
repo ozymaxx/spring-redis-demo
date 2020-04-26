f073b296c7ee29c10d3d4d30bd003389f0343436
Ozan Can Altıok <ozymaxx@gmail.com>
2020-04-26 19:40:33 +0200

    simplify tests

--- src/test/java/com/example/ozymaxx/redisdemo/configuration/RedisConfigurationTestUtilities.java
+++ src/test/java/com/example/ozymaxx/redisdemo/configuration/RedisConfigurationTestUtilities.java
@@ -1,19 +1,27 @@
 package com.example.ozymaxx.redisdemo.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
 import redis.embedded.RedisServer;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
 public class RedisConfigurationTestUtilities {
 
     private RedisServer redisServer;
 
-    public RedisConfigurationTestUtilities(int redisServerPort) {
+    public RedisConfigurationTestUtilities(@Value("${spring.cache.redis.port}") int redisServerPort) {
         this.redisServer = new RedisServer(redisServerPort);
     }
 
+    @PostConstruct
     public void launchRedisServer() {
         this.redisServer.start();
     }
 
+    @PreDestroy
     public void stopRedisServer() {
         this.redisServer.stop();
     }
--- src/test/java/com/example/ozymaxx/redisdemo/controller/ControllerAIntegrationTest.java
+++ src/test/java/com/example/ozymaxx/redisdemo/controller/ControllerAIntegrationTest.java
@@ -1,39 +1,45 @@
 package com.example.ozymaxx.redisdemo.controller;
 
 import com.example.ozymaxx.redisdemo.configuration.RedisConfigurationTestUtilities;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import com.example.ozymaxx.redisdemo.service.BackendA;
+import com.example.ozymaxx.redisdemo.service.dto.ResponseA;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = RedisConfigurationTestUtilities.class)
 @AutoConfigureMockMvc
 public class ControllerAIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ControllerA controllerA;
+
+    @SpyBean
+    private BackendA backendA;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     private static final String TEST_VALUE = "test-value";
-    private static final int REDIS_SERVER_PORT = 6370;
+    private static final String REDIS_CACHE_NAME = "sample-redis-cache";
 
-    private static RedisConfigurationTestUtilities redisConfigurationTestUtilities;
-
-    @BeforeAll
-    public static void setUp() {
-        redisConfigurationTestUtilities = new RedisConfigurationTestUtilities(REDIS_SERVER_PORT);
-        redisConfigurationTestUtilities.launchRedisServer();
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        redisConfigurationTestUtilities.stopRedisServer();
+    @BeforeEach
+    public void beforeEach() {
+        controllerA.evictCache();
     }
 
     @Test
@@ -41,5 +47,19 @@ public class ControllerAIntegrationTest {
         mockMvc.perform(get(String.format("/backendA/%s", TEST_VALUE)))
                 .andExpect(status().isOk())
                 .andExpect(content().json(String.format("{'value': '%s'}", TEST_VALUE)));
+    }
+
+    @Test
+    public void verifyRequestsCachedProperly() {
+        ResponseA responseA = controllerA.backendA(TEST_VALUE);
+        Assert.assertEquals(TEST_VALUE, responseA.getValue());
+        responseA = controllerA.backendA(TEST_VALUE);
+        Assert.assertEquals(TEST_VALUE, responseA.getValue());
+        verify(backendA, times(1)).method(TEST_VALUE);
+        Assert.assertEquals(TEST_VALUE, ((ResponseA) retrieveCachedResponse(TEST_VALUE)).getValue());
+    }
+
+    private Object retrieveCachedResponse(final String key) {
+        return redisTemplate.opsForValue().get(String.format("%s::%s", REDIS_CACHE_NAME, key));
     }
 }
